5dccf4fa1bc60e0b1a79b6f89ba86601a381daad
jg16330@students.salisbury.edu <jg16330@students.salisbury.edu@7d0edb2c-2878-11de-8010-b76041e17fd7>
2009-04-14 17:54:31 +0000

    more square updates

--- trunk/Square.java
+++ trunk/Square.java
@@ -7,7 +7,7 @@ import java.awt.*;
 public enum Square
 {
 
-	WATER(Color.BLUE, false), CARRIER(Color.RED, true), BSHIP(Color.Green, true),
+	WATER(Color.BLUE, false), CARRIER(Color.RED, true), BSHIP(Color.GREEN, true),
 	CRUISER(Color.WHITE, true), SUB(Color.MAGENTA, true), DESTROYER(Color.PINK, true);
 
 	private Color picture;    	//this could be a picture of water, or part of a ship...
@@ -27,26 +27,22 @@ public enum Square
 	Square(Color p, boolean s) {  picture = p; ship = s;}
 
 	//setters and getters
-	public Color getPicture	() { return picture; }
+	public void setPicture( Color c) { picture = c; }
+	public Color getPicture() { return picture; }
 
+	//I won't allow setters for ship
 	public boolean isShip() { return ship; }
 	public boolean isWater() { return !ship; }
 
-	public void setEnd(boolean e) { endpiece=g; }
+	public void setEnd(boolean e) { endpiece=e; }
 	public void setEnd() { endpiece=true; }
 	public boolean isEnd() { return endpiece; }
-
-
-
-
-	public void setGuessed(boolean g) { guessed=g; }
-	public void setGuessed() { guessed=true; }
-	public boolean isGuessed() { return guessed; }
 
 	public void setFacing( Direction d) { facing=d; }
 	public Direction getFacing() { return facing; }
 
-	public void setPicture( Color c) { picture = c; }
-	public Color getPicture() { return picture; }
+	public void setGuessed(boolean g) { guessed=g; }
+	public void setGuessed() { guessed=true; }
+	public boolean isGuessed() { return guessed; }
 
 }
