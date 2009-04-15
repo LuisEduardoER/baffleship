b40759a7dfc5d95bb8426b15e013a91420b20fa4
jg16330@students.salisbury.edu <jg16330@students.salisbury.edu@7d0edb2c-2878-11de-8010-b76041e17fd7>
2009-04-15 18:29:00 +0000

    client

--- trunk/Client.java
+++ trunk/Client.java
@@ -150,9 +150,9 @@ public class Client extends JFrame
    {
       try // send object to server
       {
-         output.writeObject( "CLIENT>>> " + message );
+         output.writeObject( "CHAT " + message );
          output.flush(); // flush data to output
-         displayMessage( "\nCLIENT>>> " + message );
+         displayMessage( "you: " + message );
       } // end try
       catch ( IOException ioException )
       {
