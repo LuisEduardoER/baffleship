9d41bd33ba4bc30819afb2ad9f4f2d92fc4daa7c
jg16330 <jg16330@students.salisbury.edu>
2009-05-01 17:58:05 +0000

    node

--- 370/Node.java
+++ 370/Node.java
@@ -1,29 +1,34 @@
 
-import java.awt.Point;
+import java.awt.geom.Point2D.Float;
 
 public class Node
 {
 	private boolean awake;
-	private final Point location;
+	private final Point2D.Float location;
 	private float energy;
 
 	static final float detectionRange = 5;
 	static final float maxEnergy = 100;
 
-	Node (Point p)
+	Node (Point2D.Float p)
 	{
 		awake=false;
 		energy = maxEnergy;
-		location = new Point(p);
+		location = new Point2D.Float(p);
 	}
+
+	public void wakeup() { awake=true; }
+
 
 	public void run()
 	{
 		if ( awake && (energy > 0) )
-		{ //blah blah blah
+		{ 
+			//blah blah blah
 		}
 	}
 
+	
 
 
 
