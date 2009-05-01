406d41db166c1d4e6fadb8ee34cbaa803ee1ddb9
jg16330@students.salisbury.edu <jg16330@students.salisbury.edu@7d0edb2c-2878-11de-8010-b76041e17fd7>
2009-05-01 17:48:52 +0000

    node

--- trunk/370/Node.java
+++ trunk/370/Node.java
@@ -3,6 +3,28 @@ import java.awt.Point;
 
 public class Node
 {
-private boolean awake;
-private final Point location;
+	private boolean awake;
+	private final Point location;
+	private float energy;
+
+	static final float detectionRange = 5;
+	static final float maxEnergy = 100;
+
+	Node (Point p)
+	{
+		awake=false;
+		energy = maxEnergy;
+		location = new Point(p);
+	}
+
+	public void run()
+	{
+		if ( awake && (energy > 0) )
+		{ //blah blah blah
+		}
+	}
+
+
+
+
 }
