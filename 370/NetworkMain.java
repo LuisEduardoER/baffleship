25fe11800c9a6a036ee6dfed8f7015350df1db14
fsrowe <fsrowe@7d0edb2c-2878-11de-8010-b76041e17fd7>
2009-05-04 20:50:32 +0000

    doing this every time is really gay

--- 370/NetworkMain.java
+++ 370/NetworkMain.java
@@ -10,22 +10,31 @@ import  java.io.*;
 
 public class NetworkMain extends JFrame
 {
-
-	DrawNetwork d = new DrawNetwork();
-
+    Network n = new Network();
+	DrawNetwork d;
 	NetworkMain()
 	{
 		super("tetris");		
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	        setSize(500,500);
-		//place frame in middle of screen
+	    setSize(500,500);
+		/*place frame in middle of screen
 		int x = (screenSize.width - getWidth()) / 2;  
 		int y = (screenSize.height - getHeight()) / 2;  
-
+        */
+        addNodes();
 		add(d);
-
+        setVisible(true);
 	
 	}
+	
+	public void addNodes()
+	{
+	    n.addNode("Node 1", new Point2D.Float(5, 5));
+        n.addNode("Node 2", new Point2D.Float(102, 143));
+        n.addNode("Node 3", new Point2D.Float(25, 50)); 
+        d = new DrawNetwork(n);
+
+    }
 
 
 
