40b4bc37b3c1b87ee261c269898088e1b21bc5af
fsrowe <fsrowe@7d0edb2c-2878-11de-8010-b76041e17fd7>
2009-05-04 21:10:49 +0000

    a

--- 370/NetworkMain.java
+++ 370/NetworkMain.java
@@ -10,28 +10,30 @@ import  java.io.*;
 
 public class NetworkMain extends JFrame
 {
-    Network n = new Network();
+    
 	DrawNetwork d;
 	NetworkMain()
 	{
-		super("tetris");		
+		super("Node");		
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	    setSize(500,500);
 		/*place frame in middle of screen
 		int x = (screenSize.width - getWidth()) / 2;  
 		int y = (screenSize.height - getHeight()) / 2;  
         */
-        addNodes();
+        createNetwork();
 		add(d);
         setVisible(true);
 	
 	}
 	
-	public void addNodes()
+	public void createNetwork()
 	{
+	    Network n = new Network();
 	    n.addNode("Node 1", new Point2D.Float(5, 5));
         n.addNode("Node 2", new Point2D.Float(102, 143));
         n.addNode("Node 3", new Point2D.Float(25, 50)); 
+        n.nodes.get(2).setCurrent();
         d = new DrawNetwork(n);
 
     }
