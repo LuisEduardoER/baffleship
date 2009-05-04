7fd1897eeabbf9afc163e6cea2bb361639b738a7
fsrowe <fsrowe@7d0edb2c-2878-11de-8010-b76041e17fd7>
2009-05-04 20:11:46 +0000

    paint

---
+++ 370/DrawNetwork.java
@@ -0,0 +1,27 @@
+import java.awt.*;
+import java.util.*;
+import javax.swing.*;
+import java.awt.geom.*;
+import java.awt.event.*;
+
+public class DrawNetwork extends JPanel
+{
+		
+	Network network = new Network();
+
+	DrawNetwork(){
+
+
+	}
+
+	public void paintComponent(Graphics comp)
+	{
+		super.paintComponent(comp);
+		Graphics2D comp2D = (Graphics2D)comp;
+		
+		Ellipse2D.Double node = new Ellipse2D.double(0, , 50, 50);
+		comp2D.setPaint(Color.red);
+		comp2D.fill(node);
+		
+	}
+}
