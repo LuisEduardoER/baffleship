72eda29052fd3c1f659e991e1b6baafed6fa0d10
fsrowe <fsrowe@7d0edb2c-2878-11de-8010-b76041e17fd7>
2009-05-04 21:27:37 +0000

    animation 1.0

--- trunk/370/NetworkMain.java
+++ trunk/370/NetworkMain.java
@@ -10,7 +10,7 @@ import  java.io.*;
 
 public class NetworkMain extends JFrame
 {
-    
+    Network n = new Network();
 	DrawNetwork d;
 	NetworkMain()
 	{
@@ -24,18 +24,53 @@ public class NetworkMain extends JFrame
         createNetwork();
 		add(d);
         setVisible(true);
+        alterNetwork();
 	
 	}
 	
 	public void createNetwork()
 	{
-	    Network n = new Network();
+	    
 	    n.addNode("Node 1", new Point2D.Float(5, 5));
-        n.addNode("Node 2", new Point2D.Float(102, 143));
-        n.addNode("Node 3", new Point2D.Float(25, 50)); 
-        n.nodes.get(2).setCurrent();
+        n.addNode("Node 2", new Point2D.Float(50, 50));
+        n.addNode("Node 3", new Point2D.Float(100, 100)); 
+        n.addNode("Node 3", new Point2D.Float(200, 200));
+        n.addNode("Node 4", new Point2D.Float(300, 300));
+        n.addNode("Node 5", new Point2D.Float(400, 400));
         d = new DrawNetwork(n);
-
+    }
+    
+    public void alterNetwork(){
+    
+        n.nodes.get(1).setCurrent();
+        d.repaint();
+        
+        try{Thread.sleep(1000);}
+        catch(Exception e){}
+        
+        n.nodes.get(2).setCurrent();
+        d.repaint();
+        
+        try{Thread.sleep(1000);}
+        catch(Exception e){}
+        
+        n.nodes.get(3).setCurrent();      
+        d.repaint();
+        
+        try{Thread.sleep(1000);}
+        catch(Exception e){}
+        
+         n.nodes.get(4).setCurrent();
+        d.repaint();
+        
+        try{Thread.sleep(1000);}
+        catch(Exception e){}
+        
+         n.nodes.get(5).setCurrent();
+        d.repaint();
+        
+        try{Thread.sleep(1000);}
+        catch(Exception e){}
     }
 
 
