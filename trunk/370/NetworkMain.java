f5a49582318d65a2ff5c1af5031817d118b48871
fsrowe <fsrowe@7d0edb2c-2878-11de-8010-b76041e17fd7>
2009-05-04 20:11:34 +0000

    main

---
+++ trunk/370/NetworkMain.java
@@ -0,0 +1,38 @@
+
+import java.awt.event.*;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.geom.*;
+import java.util.Random;
+
+import  java.io.*;
+
+
+public class NetworkMain extends JFrame
+{
+
+	DrawNetwork d = new DrawNetwork();
+
+	NetworkMain()
+	{
+		super("tetris");		
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	        setSize(500,500);
+		//place frame in middle of screen
+		int x = (screenSize.width - getWidth()) / 2;  
+		int y = (screenSize.height - getHeight()) / 2;  
+
+		add(d);
+
+	
+	}
+
+
+
+	public static void main(String [] args)
+	{
+		NetworkMain n = new NetworkMain();
+	}
+
+
+}
