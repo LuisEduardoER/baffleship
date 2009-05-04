167e3e43aafd97218d340cd5c8cb7375d12b89cf
jg16330 <jg16330@students.salisbury.edu>
2009-05-04 22:11:04 +0000

    server_sstuff

--- Ship.java
+++ Ship.java
@@ -6,11 +6,9 @@ import java.awt.*;
 public class Ship
 {
 	public final SquareType shipType;   //enum, shouldn't be water
-	public final Point startLocation;
-	public final Direction facing;   
-	public java.util.List<Square> squares = new ArrayList<Square>();	
-
-
+	public final Point startLocation;  //where the start of the ship is
+	public final Direction facing;    //the direction the ship points
+	public java.util.List<PositionedSquare> squares = new ArrayList<PositionedSquare>();	
 
 	public Ship(SquareType s, Point p, Direction d)
 	{
@@ -19,13 +17,9 @@ public class Ship
 		facing=d;
 		
 		for (int i=0; i<shipType.length; i++)
-		{
-			squares.add(new Square( shipType, Direction d));
+			squares.add(new PositionedSquare( shipType, d, Direction.Move(p,d,i) ));
 
-
-
-
-	;}
+	}
 
 
 
