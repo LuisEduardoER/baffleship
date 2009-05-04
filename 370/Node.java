b7a807e788af803bd6a01d500a22ca7e503c82f0
jg16330 <jg16330@students.salisbury.edu>
2009-05-04 20:11:00 +0000

    nodery

--- 370/Node.java
+++ 370/Node.java
@@ -8,16 +8,20 @@ public class Node
 	static final float maxEnergy = 100;
 
 	private Network network; //context
+	private String name; //mostly for debugging
 	private boolean awake;
-	private boolean current;  //does this node consider itself to be the "current node"?
+	private boolean current=false;  //does this node consider itself to be the "current node"?
 	public final Point2D location;
 	private float energy;		//0 or less is dead
 
-	Node (Point2D p)
+	Node (String _name, Network _network, Point2D p)
 	{
+		name=_name;
+		network = _network;
 		awake=false;
-		energy = maxEnergy;
 		location = new Point2D.Float((float)p.getX(),(float)p.getY());
+		energy = maxEnergy;
+		System.out.println("Created new node "+name+" at location "+location);
 	}
 
 	public static float distance(Point2D p, Point2D q)
@@ -35,18 +39,33 @@ public class Node
 		return ( detectionRange >= distance(p) );
 	}
 
-	public void wakeup() { if (energy>0) awake=true; }
+	public void wakeup() { if isAlive() { awake=true; System.out.println("Waking up node "+name+" at location "+location); } }
 
-	public void run()
+	public void setCurrent()
 	{
-		if ( awake && (energy > 0) )
-		{ 
-			//blah blah blah
+		if isAlive()
+		{
+			awake=true;
+			current=true;
+			System.out.println("Node "+name+" at location "+location+ " is now Current.");
 		}
 	}
 
+	public void run()
+	{
+		if ( awake && isAlive() )
+		{ 
+
+
+
+
+
+
+		}
+	}
 	
-
-
+	public boolean isAwake() { return awake; }
+	public boolean isCurrent() { return current; }
+	public boolean isAlive() { return (energy>0); }
 
 }
