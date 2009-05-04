aebeb759036d05819d2bd468df1dcfa6832e45d3
jg16330@students.salisbury.edu <jg16330@students.salisbury.edu@7d0edb2c-2878-11de-8010-b76041e17fd7>
2009-05-04 22:00:33 +0000

    ship

---
+++ trunk/Ship.java
@@ -0,0 +1,33 @@
+
+//Ship.java
+
+import java.awt.*;
+
+public class Ship
+{
+	public final SquareType shipType;   //enum, shouldn't be water
+	public final Point startLocation;
+	public final Direction facing;   
+	public java.util.List<Square> squares = new ArrayList<Square>();	
+
+
+
+	public Ship(SquareType s, Point p, Direction d)
+	{
+		shipType=s;
+		startLocation=p;
+		facing=d;
+		
+		for (int i=0; i<shipType.length; i++)
+		{
+			squares.add(new Square( shipType, Direction d));
+
+
+
+
+	;}
+
+
+
+
+}
--- trunk/SquareType.java
+++ trunk/SquareType.java
@@ -8,20 +8,21 @@ import java.awt.*;
 public enum SquareType
 {
 
-	WATER(Color.BLUE, Color.CYAN, false),
+	WATER(Color.BLUE, Color.CYAN, false,1),
 
-	CARRIER(Color.RED, Color.ORANGE, true),
-	BSHIP(Color.GREEN, Color.YELLOW, true),
-	CRUISER(Color.WHITE, Color.GRAY, true),
-	SUB(Color.MAGENTA, Color.PINK, true),
-	DESTROYER(Color.PINK, Color.RED, true);
+	CARRIER(Color.RED, Color.ORANGE, true,5),
+	BSHIP(Color.GREEN, Color.YELLOW, true,4),
+	CRUISER(Color.WHITE, Color.GRAY, true,3),
+	SUB(Color.MAGENTA, Color.PINK, true,3),
+	DESTROYER(Color.PINK, Color.RED, true,2);
 
 	public final Color picture;    	//this could be a picture of water, or part of a ship...
 	public final Color endPicture;   //alternate picture for endpieces
 	public final boolean ship;
+	public final int length;   //if its a ship, how long is the ship
 
 	//constructor
-	SquareType(Color p, Color ep, boolean s) {  picture = p; endPicture=ep; ship = s; }	
+	SquareType(Color p, Color ep, boolean s, int l) {  picture = p; endPicture=ep; ship = s; length = l}	
 	
 	public boolean isShip() { return ship; }
 	public boolean isWater() { return !ship; }
