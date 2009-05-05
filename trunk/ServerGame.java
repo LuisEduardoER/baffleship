3d2281f0132020da17792732d334c485d3a48d53
jg16330@students.salisbury.edu <jg16330@students.salisbury.edu@7d0edb2c-2878-11de-8010-b76041e17fd7>
2009-05-05 20:30:30 +0000

    more_server_stuff

--- trunk/ServerGame.java
+++ trunk/ServerGame.java
@@ -1,4 +1,5 @@
 
+import java.util.*;
 
 public class ServerGame
 {
@@ -7,24 +8,25 @@ public class ServerGame
 	private Square boardA[][];
 	private Square boardB[][];
 
+	private java.util.List<Ship> A_Ships = new ArrayList<Ship>();
+	private java.util.List<Ship> B_Ships = new ArrayList<Ship>();
+
 	private GameState gamestate;
 
 
 	public ServerGame(Server s)
 	{
 		server=s;
-
 		gamestate=GameState.WAITING;
+
 		for (int x=0;x<10;x++) for (int y=0;y<10;y++)
 		{
 			boardA[x][y] = new Square();
 			boardB[x][y] = new Square();
 		}
-		
-
 	}
 
-
+	
 
 
 
