b3250aba699aa272d9624c017fc511abc319d840
jg16330@students.salisbury.edu <jg16330@students.salisbury.edu@7d0edb2c-2878-11de-8010-b76041e17fd7>
2009-05-05 20:33:41 +0000

    more_server_stuff

---
+++ trunk/GameState.java
@@ -0,0 +1,45 @@
+
+
+//GameState.java
+
+public enum GameState
+{
+	WAITING, TURN_A, TURN_B, GAME_OVER;
+
+/*public enum Direction
+{
+
+	NORTH	{
+		Direction Clockwise() { return EAST; }
+		Direction CounterClockwise() { return WEST; }
+		Direction Opposite() { return SOUTH; }
+		},
+
+	EAST	{
+		Direction Clockwise() { return SOUTH; }
+		Direction CounterClockwise() { return NORTH; }
+		Direction Opposite() { return WEST; }
+		},
+
+	SOUTH	{
+		Direction Clockwise() { return WEST; }
+		Direction CounterClockwise() { return EAST; }
+		Direction Opposite() { return NORTH; }
+		},
+		
+	WEST	{
+		Direction Clockwise() { return NORTH; }
+		Direction CounterClockwise() { return SOUTH; }
+		Direction Opposite() { return EAST; }
+		};
+	
+
+ 	abstract Direction Clockwise(); 
+ 	abstract Direction CounterClockwise(); 
+ 	abstract Direction Opposite();
+
+}*/
+
+}
+
+
