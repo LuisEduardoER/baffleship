db0ae90305854137fe17ac21e0e38a860af1644b
jg16330@students.salisbury.edu <jg16330@students.salisbury.edu@7d0edb2c-2878-11de-8010-b76041e17fd7>
2009-05-05 20:56:36 +0000

    more_fleet_stuff

--- trunk/Ship.java
+++ trunk/Ship.java
@@ -10,7 +10,7 @@ public class Ship
 	public final SquareType shipType;   //enum, shouldn't be water
 	public final Point startLocation;  //where the start of the ship is
 	public final Direction facing;    //the direction the ship points
-	public java.util.List<PositionedSquare> squares = new ArrayList<PositionedSquare>();	
+	private java.util.List<PositionedSquare> squares = new ArrayList<PositionedSquare>();	
 
 
 public Ship(SquareType s, Point p, Direction d)
@@ -24,14 +24,15 @@ public Ship(SquareType s, Point p, Direction d)
 
 	}
 
-public boolean collide(Point p)
+public boolean collides(Point p)
 	{
 		for (int i=0; i< squares.size() ; i++) if ( squares.get(i).location.equals( p ) ) return true;
 		return false;
 	}
 
 //returns true if its a hit, false for a miss
-//but really you should probably only be calling this once you know its a hit
+//also marks the square hit
+//if you had already hit that square, congratulations, you just hit it again.
 public boolean shoot(Point p)
 	{
 		for (int i=0; i< squares.size() ; i++) if ( squares.get(i).location.equals( p ) )
