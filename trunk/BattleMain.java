ed87415eb4ebd6edb2a38197e322c15e444138af
fsrowe <fsrowe@7d0edb2c-2878-11de-8010-b76041e17fd7>
2009-05-07 22:58:27 +0000

    guishit

--- trunk/BattleMain.java
+++ trunk/BattleMain.java
@@ -7,34 +7,7 @@ import javax.swing.border.*;
 import  java.io.*;
 
 public class BattleMain extends JFrame
-{/*
-
-		BattleMain(){
-			Toolkit toolkit = Toolkit.getDefaultToolkit();  
-			Dimension screenSize = toolkit.getScreenSize();
-
-
-			JFrame frame = new JFrame("Baffleship!");		
-			BattleGUI menu = new BattleGUI();
-			
-			frame.setContentPane(menu.createContentPane());
-			frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-			//com.sun.awt.AWTUtilities.setWindowOpacity(frame, 0.70f);
-			frame.setSize(500,800);
-
-			//place frame in middle of screen
-			int x = (screenSize.width - frame.getWidth()) / 2;  
-			int y = (screenSize.height - frame.getHeight()) / 2;  
-	 		frame.setLocation(x, y); 
-			//frame.setUndecorated(true);
-			//com.sun.awt.AWTUtilities.setWindowOpaque(frame, false);
-			frame.setVisible(true);
-			Client client = new Client("127.0.0.1", 44771, menu);
-			menu.setClient(client);
-			client.runClient();
-			
-		}*/
-	
+{	
 		
 	public static void main(String[] args) {
 		Toolkit toolkit = Toolkit.getDefaultToolkit();  
@@ -47,7 +20,7 @@ public class BattleMain extends JFrame
 			frame.setContentPane(menu.createContentPane());
 			frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 			//com.sun.awt.AWTUtilities.setWindowOpacity(frame, 0.70f);
-			frame.setSize(500,800);
+			frame.setSize(800,600);
 
 			//place frame in middle of screen
 			int x = (screenSize.width - frame.getWidth()) / 2;  
