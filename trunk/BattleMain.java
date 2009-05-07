504185e5a85b6d1dc09636bbb19e1fbf1cac897c
fsrowe <fsrowe@7d0edb2c-2878-11de-8010-b76041e17fd7>
2009-05-07 19:42:43 +0000

    y

--- trunk/BattleMain.java
+++ trunk/BattleMain.java
@@ -7,8 +7,7 @@ import javax.swing.border.*;
 import  java.io.*;
 
 public class BattleMain extends JFrame
-{
-
+{/*
 
 		BattleMain(){
 			Toolkit toolkit = Toolkit.getDefaultToolkit();  
@@ -34,10 +33,48 @@ public class BattleMain extends JFrame
 			menu.setClient(client);
 			client.runClient();
 			
-		}
+		}*/
 	
 		
 	public static void main(String[] args) {
-		BattleMain b = new BattleMain();
+		Toolkit toolkit = Toolkit.getDefaultToolkit();  
+			Dimension screenSize = toolkit.getScreenSize();
+
+			Client client;
+			JFrame frame = new JFrame("Baffleship!");		
+			BattleGUI menu = new BattleGUI();
+			
+			frame.setContentPane(menu.createContentPane());
+			frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+			//com.sun.awt.AWTUtilities.setWindowOpacity(frame, 0.70f);
+			frame.setSize(500,800);
+
+			//place frame in middle of screen
+			int x = (screenSize.width - frame.getWidth()) / 2;  
+			int y = (screenSize.height - frame.getHeight()) / 2;  
+	 		frame.setLocation(x, y); 
+			//frame.setUndecorated(true);
+			//com.sun.awt.AWTUtilities.setWindowOpaque(frame, false);
+			frame.setVisible(true);
+
+			if ( args.length == 0 )
+         			client = new Client( "127.0.0.1", 44771, menu ); // connect to localhost
+     			 else
+        			 client = new Client( args[ 0 ] , 44771, menu); // use args to connect
+			//Client client = new Client("127.0.0.1", 44771, menu);
+			menu.setClient(client);
+			client.runClient();
 	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
