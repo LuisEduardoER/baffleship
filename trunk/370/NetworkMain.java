b055c604d81064c5bbd0d4bc4ace75449daeec7a
fsrowe <fsrowe@7d0edb2c-2878-11de-8010-b76041e17fd7>
2009-05-07 19:52:35 +0000

    pics

--- trunk/370/NetworkMain.java
+++ trunk/370/NetworkMain.java
@@ -10,35 +10,33 @@ import  java.io.*;
 
 public class NetworkMain extends JFrame
 {
-    Network n = new Network();
+	Network n = new Network();
 	DrawNetwork d;
 	NetworkMain()
 	{
 		super("Node");		
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	    setSize(500,500);
-		/*place frame in middle of screen
-		int x = (screenSize.width - getWidth()) / 2;  
-		int y = (screenSize.height - getHeight()) / 2;  
-        */
-        createNetwork();
+	    	setSize(500,500);
+       		createNetwork();
 		add(d);
-        setVisible(true);
-        alterNetwork();
-	
+	        setVisible(true);
+	        alterNetwork();
 	}
 	
 	public void createNetwork()
-	{
-	    
-	    n.addNode("Node 1", new Point2D.Float(5, 5));
-        n.addNode("Node 2", new Point2D.Float(50, 50));
-        n.addNode("Node 3", new Point2D.Float(100, 100)); 
-        n.addNode("Node 3", new Point2D.Float(200, 200));
-        n.addNode("Node 4", new Point2D.Float(300, 300));
-        n.addNode("Node 5", new Point2D.Float(400, 400));
-        d = new DrawNetwork(n);
-    }
+	{	    
+	    	n.addNode("Node 1", new Point2D.Float(5, 5));
+		n.addNode("Node 2", new Point2D.Float(50, 50));
+		n.addNode("Node 3", new Point2D.Float(100, 100)); 
+		n.addNode("Node 3", new Point2D.Float(200, 200));
+		n.addNode("Node 4", new Point2D.Float(300, 300));
+		n.addNode("Node 5", new Point2D.Float(400, 400));
+		n.addNode("Node 6", new Point2D.Float(100, 300));
+		n.addNode("Node 7", new Point2D.Float(300, 100));
+		n.addNode("Node 8", new Point2D.Float(200, 400));
+		n.addNode("Node 9", new Point2D.Float(400, 200));
+		d = new DrawNetwork(n);
+   	 }
     
     public void alterNetwork(){
     
@@ -48,25 +46,29 @@ public class NetworkMain extends JFrame
         try{Thread.sleep(1000);}
         catch(Exception e){}
         
+	n.nodes.get(1).setNonCurrent();
         n.nodes.get(2).setCurrent();
         d.repaint();
         
         try{Thread.sleep(1000);}
         catch(Exception e){}
         
+	n.nodes.get(2).setNonCurrent();
         n.nodes.get(3).setCurrent();      
         d.repaint();
         
         try{Thread.sleep(1000);}
         catch(Exception e){}
         
-         n.nodes.get(4).setCurrent();
+	n.nodes.get(3).setNonCurrent();
+        n.nodes.get(4).setCurrent();
         d.repaint();
         
         try{Thread.sleep(1000);}
         catch(Exception e){}
         
-         n.nodes.get(5).setCurrent();
+	n.nodes.get(4).setNonCurrent();
+        n.nodes.get(5).setCurrent();
         d.repaint();
         
         try{Thread.sleep(1000);}
