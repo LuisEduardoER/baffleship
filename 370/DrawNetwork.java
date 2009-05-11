866d261fa9c2af26b4b7caf4b46b6a1b9031df91
jg16330 <jg16330@students.salisbury.edu>
2009-05-11 21:19:21 +0000

    drawy

--- 370/DrawNetwork.java
+++ 370/DrawNetwork.java
@@ -25,12 +25,21 @@ public class DrawNetwork extends JPanel
 		super.paintComponent(comp);
 		Graphics2D comp2D = (Graphics2D)comp;
 		for(Node n : network.nodes){
-		    Ellipse2D.Double node = new Ellipse2D.Double(n.location.getX(),n.location.getY(), 30, 30);
 		    if(n.isAwake()) comp2D.setPaint(Color.red);
 		    else  comp2D.setPaint(Color.black);
 		    if(n.isCurrent()) comp2D.setPaint(Color.green);
 		    if(!n.isAlive()) comp2D.setPaint(Color.gray);
-		    comp2D.fill(node);
+		    comp2D.fill(Circle(n.location.getX(),n.location.getY(), 17));
+			comp2D.draw(Circle(n.location.getX(),n.location.getY(), 100));
 		}		
 	}
+
+	public static Ellipse2D.Double Circle(double x,double y,int r)
+	{
+		return new Ellipse2D.Double(x-r/2,y-r/2,r,r);
+		
+	}
+	
+
+
 }
